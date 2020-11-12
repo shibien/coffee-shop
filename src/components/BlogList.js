8af9623bf1f29b1714e26a00c92e8a85ff39afcb
Froszen <froszen@gmail.com>
2020-11-12 10:54:08 +0100

    new day

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -20,6 +20,9 @@ export default function BlogList(){
                             title
                             date(formatString: "MMMM D, YYYY")
                         }
+                        fields {
+                            slug
+                        }
                         excerpt
                     }
                 }
@@ -33,6 +36,7 @@ export default function BlogList(){
                 data.allMarkdownRemark.edges.map(edge => (
                     <BlogPost
                     key={edge.node.id}
+                    slug={edge.node.fields.slug}
                     title={edge.node.frontmatter.title}
                     date={edge.node.frontmatter.date}
                     excerpt={edge.node.excerpt} 
